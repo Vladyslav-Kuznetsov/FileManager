853676d7ed8d8ef8ed8d9b99fbc2b6c285897eff
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-10-08 11:07:35 +0300

    Add Interface IDirectoryItem

---
+++ FileManager/IDirectoryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NConsoleGraphics;
+
+namespace FileManager
+{
+    interface IDirectoryItem
+    {
+        string Name { get; }
+        string FullName { get; }
+        string Parent { get; }
+        string Root { get; }
+        void ShowProperties(ConsoleGraphics graphics, int coordinateX);
+        void ShowInfo(ConsoleGraphics graphics, uint color, int coordinateX);
+    }
+}
