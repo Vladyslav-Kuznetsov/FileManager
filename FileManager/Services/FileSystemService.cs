a67394cbd84d26bfd46b1895c781e73d81fb388c
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-10-20 20:01:46 +0300

    Deleted comments

--- FileManager/Services/FileSystemService.cs
+++ FileManager/Services/FileSystemService.cs
@@ -11,49 +11,7 @@ namespace FileManager.Services
 {
     public class FileSystemService
     {
-        //public void Paste(string currentPath)
-        //{
-        //    if (_clipboard.TempItem is FileItem file)
-        //    {
-        //        try
-        //        {
-        //            if (_clipboard.IsCut)
-        //            {
-        //                File.Move(file.FullName, $@"{ currentPath}\{file.Name}");
-        //                _clipboard.TempItem = null;
-        //            }
-        //            else
-        //            {
-        //                File.Copy(file.FullName, $@"{ currentPath}\{file.Name}");
-        //            }
-        //        }
-        //        catch (IOException)
-        //        {
-
-        //        }
-        //    }
-
-        //    if (_clipboard.TempItem is FolderItem directory)
-        //    {
-        //        try
-        //        {
-        //            if (_clipboard.IsCut)
-        //            {
-        //                CopyFolder(directory.FullName, $@"{currentPath}\{directory.Name}");
-        //                Directory.Delete(directory.FullName, true);
-        //                _clipboard.TempItem = null;
-        //            }
-        //            else
-        //            {
-        //                CopyFolder(directory.FullName, $@"{currentPath}\{directory.Name}");
-        //            }
-        //        }
-        //        catch (IOException)
-        //        {
-
-        //        }
-        //    }
-
+        
         public IEnumerable<SystemItem> GetFolderContent(string path)
         {
             DirectoryInfo directory = new DirectoryInfo(path);
@@ -129,7 +87,7 @@ namespace FileManager.Services
             }
         }
 
-        private static bool HasFolderPermission(DirectoryInfo directoryInfo)
+        private  bool HasFolderPermission(DirectoryInfo directoryInfo)
         {
             try
             {
